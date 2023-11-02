8db8f498b95999081045cc6d843aedadce38ce73
Marcos Rigel <marcosrigel99@gmail.com>
2023-11-02 14:51:38 -0300

    implementando a classe calculadora

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Calculadora.Services;
+
+class Program
+{
+    static void Main()
+    {
+        CalculadoraImp c = new CalculadoraImp();
+
+        int num1 = 5;
+        int num2 = 10;
+
+        System.Console.WriteLine($"{num1} + {num2} = {c.Somar(num1, num2)}");
+    }
+}
+
